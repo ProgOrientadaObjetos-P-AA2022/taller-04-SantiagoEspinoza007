e64187e08af76df9c203b54bb1001fe7cf1eff33
SantiagoEspinoza007 <esantiagoespinoza08@gmail.com>
2022-05-10 21:01:34 -0500

    Taller04 Edgar Espinoza

--- problema1/Problema1/src/paqueteDos/Ejecutor.java
+++ problema1/Problema1/src/paqueteDos/Ejecutor.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package paqueteUno;
+package paqueteDos;
 
-import paqueteDos.PromedioCalificacion;
+import paqueteUno.PromedioCalificacion;
 
 /**
  *
